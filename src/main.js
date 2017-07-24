5a0ecf51627b8c2f856a8e1d735cd7505e357f63
沈凯 <kshen@alibaba-inc.com>
2017-07-24 18:51:31 +0800

    save

--- src/main.js
+++ src/main.js
@@ -6,10 +6,27 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import '../static/css/reset.scss'
+
+//var VueTouch = require('vue-touch')
+//Vue.use(VueTouch, {name: 'v-touch'})
+
+import store from './vuex/store'
+import VueLazyLoad from 'vue-lazyload'
+
+//懒加载的默认图片 
+import def_lazy_img from '../static/img/loading.gif'
+//使用懒加载组件
+Vue.use(VueLazyLoad,{
+  loading: def_lazy_img
+})
+
+router.push('/home/popular')
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  store,
+  ...App
 })
